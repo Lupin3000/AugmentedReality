36acf3a5f196666701608f8ea391ec5c90f8ca0e
lupin <steffenlorenz@hotmail.de>
2024-12-17 16:04:54 +0100

    simplify code and remove unused code

--- example_10.py
+++ example_10.py
@@ -4,41 +4,10 @@ import cv2
 import numpy as np
 
 
-MARKER_SIZE: float = 0.035
 ARUCO_DICT_ID: int = cv2.aruco.DICT_4X4_50
-OBJ_POINTS: np.ndarray = np.array([
-        [0, 0, 0],
-        [MARKER_SIZE, 0, 0],
-        [MARKER_SIZE, MARKER_SIZE, 0],
-        [0, MARKER_SIZE, 0]
-    ], dtype=np.float32)
-FILE_PARAMS_PATH: str = "src/camera_params.npz"
 OBJECT_COLOR: tuple = (255, 255, 255)
 OBJECT_THICKNESS: int = -1
 EXAMPLE_PATH: str = "src/videos/"
-
-
-def camera_calibration(current_path: str) -> tuple:
-    """
-    Performs camera calibration by loading camera matrix and distortion
-    coefficients from a specified file path. If the file does not exist,
-    it returns default intrinsic parameters and zero distortion coefficients.
-
-    :param current_path: File path where camera parameters file is located.
-    :type current_path: str
-
-    :return: A tuple containing the camera matrix and distortion coefficients.
-    :rtype: tuple
-    """
-    param_file = join(current_path, FILE_PARAMS_PATH)
-
-    if exists(param_file):
-        print(f"[INFO] Loading camera parameters from: {param_file}")
-        params = np.load(param_file)
-        return params["camera_matrix"].astype(np.float32), params["dist_coefficients"].astype(np.float32)
-    else:
-        print("[INFO] Camera parameters file not found. Using default values.")
-        return np.array([[800, 0, 320], [0, 800, 240], [0, 0, 1]], dtype=np.float32), np.zeros(5)
 
 
 def aruco_detector() -> cv2.aruco.ArucoDetector:
@@ -68,7 +37,6 @@ if __name__ == "__main__":
 
     video_cap = cv2.VideoCapture(video_path)
 
-    matrix, coefficients = camera_calibration(current_path=current_file_path)
     detector = aruco_detector()
 
     cap = cv2.VideoCapture(0)
